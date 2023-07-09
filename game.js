5e8bc9b836645529b171bbc222e2c9bd878aebc4
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2023-07-09 04:12:08 -0500

    created keypress functionality for left and right keys to enable moving codey.

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
-const gameState = {}
+const gameState = {
+    active: true
+}
 
 function preload() {
     // load all images for gameplay
@@ -6,6 +8,14 @@ function preload() {
     this.load.image('platform', './Images/platform.png');
 };
 function create() {
+
+    // if gameState is not active than restart the scene on pointerup
+    if (gameState.active === false) {
+        this.input.on('pointerup', () => {
+          this.scene.restart();
+        })
+    };
+
 
     // create static platform
     const platforms = this.physics.add.staticGroup();
@@ -18,10 +28,23 @@ function create() {
     gameState.player.setCollideWorldBounds(true);
     this.physics.add.collider(gameState.player, platforms);
 
+    // create cursors object and save in the gameState object
+    gameState.cursors = this.input.keyboard.createCursorKeys();
 };
 function update() {
-    
-}
+    // if gameState is active than players can control codey with the keyboard.
+    if (gameState.active) {
+        // check which keys are being pressed
+        if (gameState.cursors.left.isDown) {
+            gameState.player.setVelocityX(-160);
+        } else if (gameState.cursors.right.isDown) {
+            gameState.player.setVelocityX(160);
+        } else {
+            gameState.player.setVelocityX(0);
+        }
+    }
+
+};
 
 const config = {
     type: Phaser.AUTO,
