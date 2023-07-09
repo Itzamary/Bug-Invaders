31d46cbdb8ef04acba78f13a0c7301f511a90174
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2023-07-09 03:52:15 -0500

    added player and platform images to be used in the javascript file, created sprites and static group for the player and platform created collision rules between world and player and betweeen player and platform.

--- game.js
+++ game.js
@@ -1,12 +1,41 @@
-function preload() {};
-function create() {};
-function update() {}
+const gameState = {}
+
+function preload() {
+    // load all images for gameplay
+    this.load.image('codey', './Images/codey.png');
+    this.load.image('platform', './Images/platform.png');
+};
+function create() {
+
+    // create static platform
+    const platforms = this.physics.add.staticGroup();
+    platforms.create(225, 490, 'platform').setScale(1, .3).refreshBody();
+
+    // create player
+    gameState.player = this.physics.add.sprite(225, 450, 'codey').setScale(.5);
+
+    // create collider between player and platform.
+    gameState.player.setCollideWorldBounds(true);
+    this.physics.add.collider(gameState.player, platforms);
+
+};
+function update() {
+    
+}
 
 const config = {
     type: Phaser.AUTO,
-    width: 350,
-    height: 200, 
+    width: 450,
+    height: 500, 
     backgroundColor: 'b9eaff',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 200},
+            enableBody: true
+        }
+  
+    },
     scene: {
         preload,
         create,
