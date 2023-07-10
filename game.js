dc375a55e54e35d87aee22b36640121051361bec
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2023-07-10 03:43:41 -0500

    added player bullet and created functionality for when the user clicks the space bar to shoot. created collistion rules for the player bullet and bug and for the player bullet and bug bullet.

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ function preload() {
     this.load.image('platform', './Images/platform.png');
     this.load.image('bug1', './Images/bug_1.png');
     this.load.image('bugPellet', './Images/bugPellet.png');
+    this.load.image('bugRepellent', './Images/bugRepellent.png');
 };
 function create() {
     // gameState.active = true;
@@ -32,8 +33,8 @@ function create() {
     // create enemy key as group object
     gameState.enemies = this.physics.add.group();
     // create for loop to generate enemies
-    for (yValue = 1; yValue < 4; yValue++) {
-        for (xValue = 1; xValue < 9; xValue++) {
+    for (let yValue = 1; yValue < 4; yValue++) {
+        for (let xValue = 1; xValue < 9; xValue++) {
             // create the enemies in 3 rows and 8 columns
             // set scale to 60% and set gravity so the bugs arent pulled down.
             gameState.enemies.create(50 * xValue, 50 * yValue, 'bug1').setScale(.6).setGravityY(-200);
@@ -57,6 +58,9 @@ function create() {
         callbackScope: this,
         loop: true
     });
+
+    // create group of player bullets.
+    gameState.playerBullet = this.physics.add.group();
 
 
     // create collider between player and platform.
@@ -82,7 +86,24 @@ function create() {
 
         // add text so the player knows its game over.
         this.add.text(210, 250, 'Game Over \n Click to start New Game.', { fontSize: '15px', fill: '#000000'});
+    });
+
+    // create collider between player bullet and enemy
+    this.physics.add.collider(gameState.enemies, gameState.playerBullet, (bug, bullet) => {
+        // destroy the bug
+        bug.destroy();
+        // destroy the bullet
+        bullet.destroy();
+    });
+
+    // create collider between player bullet and enemy bullet.
+    this.physics.add.collider(gameState.playerBullet, bugBullet, (playerBullet, bugBullet) => {
+        // destroy the bullet for each the playet and bug.
+        playerBullet.destroy();
+        bugBullet.destroy();
     })
+
+
     // create cursors object and save in the gameState object
     gameState.cursors = this.input.keyboard.createCursorKeys();
 };
@@ -96,10 +117,16 @@ function update() {
             gameState.player.setVelocityX(160);
         } else {
             gameState.player.setVelocityX(0);
+        };
+
+        // check if the space key is being pressed so the player can shoot back.
+        if (Phaser.Input.Keyboard.JustDown(gameState.cursors.space)) {
+            // create bullets on the same location as the player. and change its gravity so it shoots upward
+            gameState.playerBullet.create(gameState.player.x, gameState.player.y, 'bugRepellent').setGravityY(-400);
         }
     }
 
-};
+};   
 
 const config = {
     type: Phaser.AUTO,
